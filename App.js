8ca34e1a6e975c61da0117ea0ed64225634c3aaa
wemeet-arqhive <arqhive@wemeetplace.com>
2020-01-31 17:04:39 +0900

    FlatList

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import StatusBar from './components/StatusBar';
+import FlatList from './components/FlatList';
 
 export default class App extends Component {
   render() {
-    return <StatusBar />;
+    return <FlatList />;
   }
 }
--- components/FlatList.js
+++ components/FlatList.js
@@ -1,5 +1,22 @@
 import React from 'react';
-import {SafeAreaView, View, FlatList, StyleSheet, Text} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+} from 'react-native';
+
+// method
+// scrollToEnd([params])
+// scrollToIndex(params)
+// scrollToItem(params)
+// scrollToOffset(paramse);
+// recordInteraction() 상호 작용이 발생한 목록
+// flashScrollIndicators() 스크롤 인디케이터를 잠깐 표시
+// getScrollReponder()
+// getScrollableNode()
 
 const DATA = [
   {
@@ -16,11 +33,15 @@ const DATA = [
   },
 ];
 
-function Item({title}) {
+function Item({title, separators}) {
   return (
-    <View style={styles.item}>
+    <TouchableHighlight
+      style={styles.item}
+      onShowUnderlay={separators.highlight}
+      onHideUnderlay={separators.unhighlight}
+    >
       <Text style={styles.title}>{title}</Text>
-    </View>
+    </TouchableHighlight>
   );
 }
 
@@ -28,9 +49,40 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={DATA}
-        renderItem={({item}) => <Item title={item.title} />}
+        data={DATA} // plain array
+        renderItem={({item, index, separators}) => (
+          <Item title={item.title} separators={separators} />
+        )}
+        ItemSeparatorComponent={separators => <View style={{borderWidth: 1}} />}
+        ListEmptyComponent={() => <View />} // 목록이 비었을때 표시되는 컴포넌트
+        ListFooterComponent={() => (
+          <View>
+            <Text>Footer</Text>
+          </View>
+        )} // 리스트 가장 하단에 렌더됨
+        ListFooterComponentStyle={{backgroundColor: 'red'}}
+        ListHeaderComponent={() => (
+          <View>
+            <Text>Header</Text>
+          </View>
+        )}
+        ListHeaderComponentStyle={{backgroundColor: 'blue'}}
         keyExtractor={item => item.id}
+        // columnWrapperStyle
+        // extraData 외부 데이터에 반응하여 리스트가 다시 그려져야 한다면..
+        // getItemLayout={(data, index) => )}
+        // horizontal
+        // initialNumToRender={10} // 초기 마운트 렌더링수
+        // initialScrollIndex={1} getItemLayout과 같이 구현해야 한다.
+        // inverted={true} // 스크롤 방향을 반대로 바꾼다.
+        // numColumns={2} // 열수
+        // onEndReached={() => alert('스크롤이 바닥에 닿았을때')}
+        // onEndReachedThreshold={0.5} // 바닥에서 닿을때 오프셋
+        progressViewOffset={1} // Android Only. 프로그레스 뷰 오프셋
+        // onViewableItemsChanged 아래의 viewabilityConfig와 같이 사용하는데 사용할 일이 크게 있을거 같지 않다.
+        // viewabilityConfig
+        refreshing
+        // removeClippedSubviews 이걸 true로 설정하면 스크롤 성능이 향상 될 수 있으나 콘텐츠 누락이 생길 수 있음
       />
     </SafeAreaView>
   );
