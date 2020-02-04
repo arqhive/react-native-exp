51edaefe4be2c62ace3038e65c9e0ef23f8c4418
wemeet-arqhive <arqhive@wemeetplace.com>
2020-02-04 11:58:01 +0900

    Improving user experience

---
+++ Guide/ImprovingUserExperience.js
@@ -0,0 +1,16 @@
+// 텍스트인풋구성
+// 1. 첫 번째 필드를 오토 포커스 하기
+// 2. 예상 데이터 형식을 지정
+// 3. 자동 고침 활성화 및 비활성화
+// 4. 키보드 유형 설정
+// 5. 리턴키를 눌렀을때 행동할 동작 올바르게 지정
+
+// 키보드가 표시될때 레이아웃 올바르게 지정 KeyboardAvoidingView
+
+// 인터랙션 컴포넌트가 44x44이상 이 되게 설정 (hitSlop)
+
+// 안드로이드 리플 사용 (TouchableNativeFeedback)
+
+// 화면 잠금
+// iOS는 info.plist에서 구성
+// android:screenOrientation="landscape" 안드로이드
