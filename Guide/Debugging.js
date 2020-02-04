c11672629f017f14a5e95dcfa05f8f5dff89bbc0
wemeet-arqhive <arqhive@wemeetplace.com>
2020-02-04 12:16:01 +0900

    Debugging

---
+++ Guide/Debugging.js
@@ -0,0 +1,13 @@
+import {YellowBox} from 'react-native';
+
+// console.error() -> Red Box
+// console.warn() -> Yellow Box
+YellowBox.ignoreWarnings(['이 항목은 무시된다..']);
+
+// react-devtools
+// yarn global add react-devtools
+// 버전을 잘 맞춰서 사용해야 한다.
+
+// 앱이 실행되는 동안
+// react-native log-ios
+// react-native log-android
